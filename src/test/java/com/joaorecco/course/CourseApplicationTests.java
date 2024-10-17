ed41a03667347759f1e1aa8e1027758ca3a9137a
João Alves <reccoalves10@gmail.com>
2024-10-17 15:15:03 -0300

    project created

---
+++ src/test/java/com/joaorecco/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaorecco.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
